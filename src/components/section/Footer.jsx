7a251d4396453fdea95e0cd08625a464234c4244
deliondane <hynnk0@gmail.com>
2024-11-21 15:11:28 +0900

    1121

---
+++ src/components/section/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer id='footer' role='contentinfo'>
+      <a href='mailto:jmk255@naver.com' rel='noopener noreferrer'>
+        hynnk0@gmail.com
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
